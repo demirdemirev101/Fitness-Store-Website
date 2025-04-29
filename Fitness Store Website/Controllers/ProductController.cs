121bcc536ed53e96d89a04891abb0953328dcbac
demir <demirdemirev101@gmail.com>
2025-04-29 12:58:41 +0300

    Added filters by name, price and category dinamically.

--- Fitness Store Website/Controllers/ProductController.cs
+++ Fitness Store Website/Controllers/ProductController.cs
@@ -12,19 +12,41 @@ namespace Fitness_Store_Website.Controllers
         {
             data = _data;
         }
-        public async Task<IActionResult> All()
+        public async Task<IActionResult> All(int? categoryId, string? sortByName, string? sortByPrice)
         {
-            var allProducts = await data.Products
-                .Select(x => new ProductsViewModel
-                {
-                    Name=x.Name,
-                    Price=x.Price,
-                    URL=x.URL
-                })
-                .ToListAsync();
+            var productsQuery = data.Products.AsQueryable();
+
+            if (categoryId!=null)
+            {
+                productsQuery = productsQuery
+                                .Where(p => p.CategoryId == categoryId.Value);
+            }
+
+            if (!string.IsNullOrEmpty(sortByName))
+            {
+                productsQuery = sortByName == "asc"
+                    ? productsQuery.OrderBy(p => p.Name)
+                    : productsQuery.OrderByDescending(p => p.Name);
+            }
+
+            if (!string.IsNullOrEmpty(sortByPrice))
+            {
+                productsQuery = sortByPrice == "asc"
+                    ? productsQuery.OrderBy(p => p.Price)
+                    : productsQuery.OrderByDescending(p => p.Price);
+            }
+
+            var allProducts = await productsQuery
+                         .Select(x => new ProductsViewModel
+                         {
+                             Name = x.Name,
+                             Price = x.Price,
+                             URL = x.URL
+                         })
+                         .ToListAsync();
 
             return View(allProducts);
         }
-        
+
     }
 }
